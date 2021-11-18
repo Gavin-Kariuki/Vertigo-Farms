63645d11bd0ff3bcde43adbdc4608f620369f8f3
kennedybabu <kennedynjogu8@gmail.com>
2021-11-18 09:09:43 +0300

    feature: create the email.py file

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 export MAIL_USERNAME='yourEmail@domainName.com' # change to your liking, though it is not a must you have this here.
-
+export MAIL_PASSWORD=''
 #This is important once you clone the repo to automate the process of running start.sh
 python manage.py db init
 python manage.py db migrate
